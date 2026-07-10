a5b07e243086a124b5f28c235c3ba0b9fd0bc2cc
Harshvardhan Bohare <157799269+hbohare2004@users.noreply.github.com>
2026-07-10 23:41:03 +0530

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int minSpeed = 1;
+
+        int maxSpeed = 0;
+        for(int pile : piles){
+            maxSpeed = Math.max(maxSpeed, pile);
+        }
+
+        // binary s
+        while(minSpeed < maxSpeed){
+            int mid = minSpeed + (maxSpeed - minSpeed) / 2;
+
+            if(canEat(piles, h, mid)){
+                maxSpeed = mid;
+            } else{
+                minSpeed = mid +1;
+            }
+        }
+        return maxSpeed;
+    }
+
+    private boolean canEat(int piles[], int h, int speed){
+        int hours = 0;
+        for(int pile : piles){
+            hours += (int) Math.ceil((double) pile / speed);
+        }
+        return hours <= h;
+    }
+}
